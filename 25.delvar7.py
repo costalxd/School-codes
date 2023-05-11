2a056d106b68d2eabe56ae8e03cfc41872e8537b
costalxd <serafim.sergeev.joj@mail.ru>
2023-05-11 20:34:34 +0300

    codes

--- 25.delvar7.py
+++ 25.delvar7.py
@@ -1,10 +1,10 @@
-start=860000
+start=800000
 count=1
-for ch in range(start, start+100000):
-    for i in range(2,1000):
+for ch in range(start, 1, -1):
+    for i in range(2, 1000):
         if ch%i==0:
             M=(ch//i)-i
-            if M%100==18 and count<=5:
+            if M%17==0 and M!=0 and count<=5:
                 count+=1
                 print(ch, M)
             break
