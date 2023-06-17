8fd1b076b332ab3a7ddac0facd97586651adc879
costalxd <serafim.sergeev.joj@mail.ru>
2023-06-17 23:56:24 +0300

    codes

--- 15.var13.py
+++ 15.var13.py
@@ -2,8 +2,9 @@ for A in range(0,1000):
     f=0
     for x in range(0,1000):
         for y in range(0,1000):
-            if ( (x>=A) or (y>=A) or ((x*y) <= 200) ) == 1:
-                f+=1
-    if f==1000*1000:
+            if ( (x>=A) or (y>=A) or ((x*y) <= 200) ) == 0:
+                f=1
+                break
+    if f==0:
         print(A)
 
