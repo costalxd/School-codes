2b0552bed2fdc5d6e7f4f893b35ae9b1832bc825
costalxd <serafim.sergeev.joj@mail.ru>
2023-06-18 22:24:31 +0300

    codes

--- 12.var20.py
+++ 12.var20.py
@@ -1,17 +1,14 @@
 import random
-
 def prost(n):
     for i in range(2, n):
         if n%i == 0:
             return 0
     return 1
-
 for n in range(1, 100):
     a = list(f"{23*'1'+n*'2'+25*'3'}")
     random.shuffle(a)
     a = ''.join(a)
     a = f">{a}"
-
     for _ in range(10000):
         a = a.replace('>1', '1>', 1)
         a = a.replace('>2', '>3', 1)
