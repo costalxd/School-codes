ab83fe63958a33ab1579f5eb5311698cba6074bf
costalxd <serafim.sergeev.joj@mail.ru>
2023-06-18 22:10:59 +0300

    codes

--- 25.del.var20.py
+++ 25.del.var20.py
@@ -3,14 +3,12 @@ def prost(n):
         if n%x == 0:
             return False
     return True
-
-def prdl(n):
+def prdl(n): #проверка делителей
     A = 0
     for x in range(2, n//2+1):
         if n%x == 0 and prost(x):
             A+=x
     return A
-
 start=550000
 count=1
 for ch in range(start+1, start+100001):
