a4b633a9b7d1113e3131f8a446df802c55f4ff66
costalxd <serafim.sergeev.joj@mail.ru>
2023-06-18 21:57:50 +0300

    codes

---
+++ 25.del.var20.py
@@ -0,0 +1,20 @@
+def prost(n):
+    for x in range(2, n):
+        if n%x == 0:
+            return False
+    return True
+
+def prdl(n):
+    A = 0
+    for x in range(2, n//2+1):
+        if n%x == 0 and prost(x):
+            A+=x
+    return A
+
+start=550000
+count=1
+for ch in range(start+1, start+100001):
+    sumdel=prdl(ch)
+    if sumdel%10==7 and count<=5:
+        count+=1
+        print(ch, sumdel)
